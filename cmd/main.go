46848bb5c9cb6cd717ce6a3a51ff4523e14618d0
dongzerun <xueling1000@sina.com>
2015-11-13 18:02:59 +0800

    remove http pprof

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"flag"
-	"net/http"
-	_ "net/http/pprof"
-	"runtime"
-	"time"
 
 	"github.com/dongzerun/archer"
-	log "github.com/ngaut/logging"
 )
 
 var (
@@ -17,19 +12,7 @@ var (
 
 func main() {
 	flag.Parse()
-
 	pc := archer.NewProxyConfig(*cfg)
-
-	go func() {
-		for {
-			log.Info("Got goroutine ", runtime.NumGoroutine())
-			time.Sleep(1000 * time.Millisecond)
-		}
-	}()
-
-	go func() {
-		log.Info(http.ListenAndServe("0.0.0.0:6061", nil))
-	}()
 	p := archer.NewProxy(pc)
 	p.Start()
 }
